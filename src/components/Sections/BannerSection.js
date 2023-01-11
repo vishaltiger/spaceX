c8a43090c7d7d65e00335cbce1ee724593ddf3bb
vishal.s <vishal.s@technovert.com>
2023-01-11 14:11:10 +0530

    initial commit

---
+++ src/components/Sections/BannerSection.js
@@ -0,0 +1,15 @@
+
+
+function BannerSection() {
+  return (
+    <main className='container mx-auto px-6 pt-16 flex-1 text-center mt-20 backdrop-blur-sm'>
+      <h2 className="md:text-2xl lg:text-4xl uppercase">
+        Welcome to
+      </h2>
+      <h2 className="md:text-4xl lg:text-6xl uppercase text-slate-200 font-bold mb-8 spacexText">
+        SpaceX
+      </h2>
+    </main>
+  )
+}
+export default BannerSection;
